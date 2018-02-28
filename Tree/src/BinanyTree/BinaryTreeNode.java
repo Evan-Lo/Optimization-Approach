097a1592d25beab1468c32430d401566a147ca5f
LOEVAN <donkeyjeorge@gmail.com>
2018-02-28 21:11:40 +0800

    Update Tree

---
+++ Tree/src/BinanyTree/BinaryTreeNode.java
@@ -0,0 +1,25 @@
+package BinanyTree;
+import java.util.Arrays;
+
+public class BinaryTreeNode {
+	Object element;
+	int[] element2;
+	BinaryTreeNode leftChild;
+	BinaryTreeNode rightChild;
+
+	public BinaryTreeNode() {
+	}
+
+	public BinaryTreeNode(Object theElement,int[] theElement2) {
+		element = theElement;
+		element2= theElement2;
+	}
+
+	public BinaryTreeNode(Object theElement,BinaryTreeNode theleftChild,BinaryTreeNode therightChild)
+	{
+		element= theElement;
+		leftChild=theleftChild;
+		rightChild=therightChild;
+	}
+
+}
